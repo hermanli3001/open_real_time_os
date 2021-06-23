0746a2b33680e7a2f98c1c354da986455239a89d
hehel <hehe.li.sdu@outlook.com>
2021-06-23 16:47:09 +0800

    update files and folders

---
+++ kernel/include/math/data_type.h
@@ -0,0 +1,15 @@
+//
+// Created by hehel on 2021/6/23.
+//
+
+#ifndef OPEN_REAL_TIME_OS_DATA_TYPE_H
+#define OPEN_REAL_TIME_OS_DATA_TYPE_H
+
+typedef unsigned int UINT;
+typedef int INT64;
+
+
+
+
+
+#endif //OPEN_REAL_TIME_OS_DATA_TYPE_H
